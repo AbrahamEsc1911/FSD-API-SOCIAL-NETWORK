693923290b65ec7323bbd9d03e897fad635daeb3
AEscobarAngola <abrancho1908@gmail.com>
2024-07-17 19:59:02 +0200

    feat:middleware auth created and added to some endpoints

--- src/entities/users/users.routers.js
+++ src/entities/users/users.routers.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getAllUsers, login, register } from "./users.controllers.js";
+import { getAllUsers, login, register, userProfile } from "./users.controllers.js";
+import { auth } from "../../middlewares/auth.js";
 
 const router = Router()
 
 router.post('/register', register)
 router.post('/login', login)
 router.get('/', getAllUsers)
+router.get('/profile', auth, userProfile)
 
 export {router}
---
+++ src/middlewares/auth.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken'
+
+export const auth = (req, res, next) => {
+    try {
+       
+        if(!req.headers.authorization){
+            res.status(401).json(
+                {
+                    success: false,
+                    message: 'Unautorized'
+                }
+            )
+        }
+
+        const token = req.headers.authorization.split(' ')[1];
+       
+        const decode = jwt.verify(token, process.env.SECRET_KEY)
+
+        req.tokenData = {
+            id: decode.id,
+            roles: decode.role,
+            email: decode.email
+        }
+        console.log(req.tokenData.roles)
+        next();
+        
+    } catch (error) {
+        res.status(500).json(
+            {
+                success: false,
+                message: 'Unauthorized'
+            }
+        )
+    }
+}
