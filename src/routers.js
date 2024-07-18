1d8f6ca481b03edfeb6f5040651bb2a0c898a3d4
AEscobarAngola <abrancho1908@gmail.com>
2024-07-18 16:31:29 +0200

    feat: posts routers created

--- src/routers.js
+++ src/routers.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { router as UserRouter} from './entities/users/users.routers.js'
+import { router as postsRouter} from './entities/posts/posts.routers.js'
 
 
 const router = Router()
 
 router.use('/users', UserRouter)
+router.use('posts', postsRouter)
 
 export {router}
