3cfdd545a44445fa796b68e973ba04606be072b4
AEscobarAngola <abrancho1908@gmail.com>
2024-07-18 17:42:38 +0200

    feat: delete post by id controller created

--- src/entities/posts/posts.controller.js
+++ src/entities/posts/posts.controller.js
@@ -1,8 +1,9 @@
+import { isValidObjectId } from "mongoose"
 import Posts from "./posts.model.js"
 
 export const createPost = async (req, res) => {
     try {
-        
+
         const id = req.tokenData.id
         const message = req.body.message
 
@@ -41,5 +42,48 @@ export const createPost = async (req, res) => {
 }
 
 export const deletePost = async (req, res) => {
-    
+    try {
+        const postId = req.params.id
+
+        if (!isValidObjectId(postId)) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: 'Post Id is not valid'
+                }
+            )
+        }
+
+        const postDeleted = await Posts.deleteOne(
+            {
+                _id: postId
+            }
+        )
+
+        if (postDeleted.deletedCount === 0) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: 'any post to delete with that id'
+                }
+            )
+        }
+
+        res.json(
+            {
+                success: true,
+                message: 'Post deleted',
+                data: postDeleted
+            }
+        )
+
+    } catch (error) {
+        res.status(500).json(
+            {
+                success: false,
+                message: 'Error deleting post',
+                error: error.message
+            }
+        )
+    }
 }
--- src/entities/posts/posts.routers.js
+++ src/entities/posts/posts.routers.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { auth } from "../../middlewares/auth.js";
-import { createPost } from "./posts.controller.js";
+import { createPost, deletePost } from "./posts.controller.js";
 
 const router = Router()
 
