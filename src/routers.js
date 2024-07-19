ef9b2c080848e8e37b846d1b8a4bdf3e234df756
AEscobarAngola <abrancho1908@gmail.com>
2024-07-19 16:10:10 +0200

    feat: comments router created

--- src/routers.js
+++ src/routers.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { router as UserRouter} from './entities/users/users.routers.js'
 import { router as postsRouter} from "./entities/posts/posts.routers.js";
+import { router as commentsRouter } from "./entities/comments/comments.routers.js";
 
 
 const router = Router()
 
 router.use('/users', UserRouter)
 router.use('/posts', postsRouter)
+router.use('/comments', commentsRouter)
 
 export {router}
