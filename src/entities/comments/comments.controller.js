4627c76e99cefa87c748e6f4545e8bf71da8fc7e
AEscobarAngola <abrancho1908@gmail.com>
2024-07-19 16:14:30 +0200

    feat: create comments endpoint created

---
+++ src/entities/comments/comments.controller.js
@@ -0,0 +1,7 @@
+export const createComment = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- src/entities/comments/comments.routers.js
+++ src/entities/comments/comments.routers.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { auth } from "../../middlewares/auth";
+import { createComment } from "./comments.controller";
 
 const router = Router()
+
+router.post('/', auth, createComment)
 
 export { router }
